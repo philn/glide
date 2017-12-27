39960af5f6422aaf4a6fde96e2099694deb6f162
Philippe Normand <phil@base-art.net>
2017-12-27 16:52:00 +0000

    Add fullscreen action & shortcut
    Press META-f to enter fullscreen mode. Same (or escape) to exit.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ extern crate gstreamer as gst;
 extern crate gstreamer_player as gst_player;
 extern crate gstreamer_video as gst_video;
 extern crate gtk;
+#[macro_use]
+extern crate lazy_static;
 extern crate send_cell;
 
 use cairo::Context as CairoContext;
@@ -19,12 +21,21 @@ use std::env;
 use std::os::raw::c_void;
 use std::process;
 use std::sync::Arc;
+use std::sync::Mutex;
 
 #[derive(Clone)]
 struct VideoPlayer {
     player: gst_player::Player,
     renderer: gst_player::PlayerVideoOverlayVideoRenderer,
+    window: gtk::Window,
     video_area: gtk::DrawingArea,
+    fullscreen_action: gio::SimpleAction,
+}
+
+lazy_static! {
+    static ref INHIBIT_COOKIE: Mutex<Option<u32>> = {
+        Mutex::new(None)
+    };
 }
 
 impl VideoPlayer {
@@ -39,12 +50,21 @@ impl VideoPlayer {
         );
 
         let video_area = gtk::DrawingArea::new();
-        video_area.set_double_buffered(false);
+
+        let fullscreen_action =
+            gio::SimpleAction::new_stateful("fullscreen", None, &false.to_variant());
+        gtk_app.add_action(&fullscreen_action);
+
+        let window = gtk::Window::new(gtk::WindowType::Toplevel);
+        window.set_default_size(320, 240);
+        window.set_resizable(true);
 
         let video_player = VideoPlayer {
             player,
             renderer,
+            window,
             video_area,
+            fullscreen_action,
         };
         let myself = Arc::new(video_player);
 
@@ -89,7 +109,50 @@ impl VideoPlayer {
                 });
         }
 
+        {
+            let app_clone = gtk_app.clone();
+            let self_clone = Arc::clone(&myself);
+            myself.window.connect_key_press_event(move |_, key| {
+                let keyval = key.get_keyval();
+                let keystate = key.get_state();
+                let app = &app_clone;
+
+                if keystate.intersects(gdk::ModifierType::META_MASK) {
+                    if keyval == gdk::enums::key::f {
+                        self_clone.toggle_fullscreen(app, true);
+                    }
+                } else if keyval == gdk::enums::key::Escape {
+                    self_clone.toggle_fullscreen(app, false);
+                }
+
+                Inhibit(false)
+            });
+        }
+
         myself
+    }
+
+    pub fn toggle_fullscreen(&self, app: &gtk::Application, allowed: bool) {
+        let fullscreen_action = &self.fullscreen_action;
+        let window = &self.window;
+        if let Some(is_fullscreen) = fullscreen_action.get_state() {
+            let fullscreen = is_fullscreen.get::<bool>().unwrap();
+            if fullscreen {
+                if let Ok(mut cookie) = INHIBIT_COOKIE.lock() {
+                    app.uninhibit(cookie.unwrap());
+                    *cookie = None;
+                }
+                window.unfullscreen();
+                window.present();
+                fullscreen_action.change_state(&(!fullscreen).to_variant());
+            } else if allowed {
+                let flags =
+                    gtk::ApplicationInhibitFlags::SUSPEND | gtk::ApplicationInhibitFlags::IDLE;
+                *INHIBIT_COOKIE.lock().unwrap() = Some(app.inhibit(window, flags, None));
+                window.fullscreen();
+                fullscreen_action.change_state(&(!fullscreen).to_variant());
+            }
+        }
     }
 
     pub fn prepare_video_overlay(&self) {
@@ -176,20 +239,15 @@ impl VideoPlayer {
     }
 
     pub fn start(&self, app: &gtk::Application) {
-        let window = gtk::Window::new(gtk::WindowType::Toplevel);
-        window.set_default_size(320, 240);
-        window.set_resizable(true);
-
         let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
         vbox.pack_start(&self.video_area, true, true, 0);
 
         let label = gtk::Label::new("Position: 00:00:00");
         vbox.pack_start(&label, false, false, 5);
-        window.add(&vbox);
+        self.window.add(&vbox);
 
-        window.show_all();
-
-        app.add_window(&window);
+        self.window.show_all();
+        app.add_window(&self.window);
 
         let label_clone = SendCell::new(label.clone());
         self.player.connect_position_updated(move |_, position| {
@@ -205,7 +263,7 @@ impl VideoPlayer {
             });
 
         let app_clone = app.clone();
-        window.connect_delete_event(move |_, _| {
+        self.window.connect_delete_event(move |_, _| {
             let app = &app_clone;
             app.quit();
             Inhibit(false)
