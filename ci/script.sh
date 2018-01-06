c1a126627f4107371412e3f8e8bc17f8252f456f
Philippe Normand <phil@base-art.net>
2018-01-06 17:08:12 +0000

    Travis build fix attempt #1 or 999
    Perhaps this will allow cairo-rs to build. I have no idea!

--- ci/script.sh
+++ ci/script.sh
@@ -12,7 +12,7 @@ main() {
     #fi
 
     #cross test --target $TARGET
-    cross test --target $TARGET --release
+    PKG_CONFIG_ALLOW_CROSS=1 cross test --target $TARGET --release
 
     #cross run --target $TARGET
     #cross run --target $TARGET --release
