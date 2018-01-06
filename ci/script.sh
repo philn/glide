a49395f3448f2e12c4e49f8b0b4a1d8019a3c94f
Philippe Normand <phil@base-art.net>
2018-01-06 17:50:47 +0000

    Travis blah blah

--- ci/script.sh
+++ ci/script.sh
@@ -4,8 +4,8 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    #cross build --target $TARGET
+    PKG_CONFIG_ALLOW_CROSS=1 cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
