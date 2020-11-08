2d006a0399bf8e2f7bf91e1d7431a8033116dae8
Philippe Normand <phil@base-art.net>
2020-11-08 10:27:57 +0000

    main: Look for more sub file extensions when loading external subs

--- src/constants.rs
+++ src/constants.rs
@@ -4,3 +4,5 @@
 
 pub static SEEK_BACKWARD_OFFSET: gst::ClockTime = gst::ClockTime(Some(2_000_000_000));
 pub static SEEK_FORWARD_OFFSET: gst::ClockTime = gst::ClockTime(Some(5_000_000_000));
+
+pub static SUB_FILE_EXTENSIONS: [&str; 3] = ["srt", "sub", "ass"];
