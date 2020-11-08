42040444bfba0eb2816712b6a66372bf505c50c4
Philippe Normand <phil@base-art.net>
2020-11-08 10:23:19 +0000

    main: Move constants to a standalone module

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+// Only possible in nightly
+// static SEEK_BACKWARD_OFFSET: gst::ClockTime = gst::ClockTime::from_mseconds(2000);
+// static SEEK_FORWARD_OFFSET: gst::ClockTime = gst::ClockTime::from_mseconds(5000);
+
+pub static SEEK_BACKWARD_OFFSET: gst::ClockTime = gst::ClockTime(Some(2_000_000_000));
+pub static SEEK_FORWARD_OFFSET: gst::ClockTime = gst::ClockTime(Some(5_000_000_000));
