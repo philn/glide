e484e3100ee1c010ffce5b39b7d3855d8573a332
Philippe Normand <phil@base-art.net>
2023-12-07 10:01:42 +0000

    about: Display devel version

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
-fn main() {
-    let target = std::env::var("TARGET").unwrap();
+use std::error::Error;
+use vergen::EmitBuilder;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let target = std::env::var("TARGET")?;
     if target.contains("linux") {
         println!("cargo:rustc-link-lib=X11");
     } else if target.contains("darwin") {
         println!("cargo:rustc-link-lib=framework=IOKit");
     }
+
+    Ok(EmitBuilder::builder().all_build().all_git().emit()?)
 }
