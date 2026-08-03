c7320a663172d6298a571ad0f57a8b90c819734e
Philippe Normand <phil@base-art.net>
2026-08-03 14:40:03 +0100

    Add i18n infrastructure and French translation
    Fixes #130

---
+++ src/i18n.rs
@@ -0,0 +1,11 @@
+use crate::config;
+use gettextrs::*;
+
+pub fn init() {
+    setlocale(LocaleCategory::LcAll, "");
+    if let Some(localedir) = config::localedir() {
+        bindtextdomain(config::gettext_package(), localedir).expect("Unable to bind the text domain");
+        bind_textdomain_codeset(config::gettext_package(), "UTF-8").expect("Unable to set text domain encoding");
+        textdomain(config::gettext_package()).expect("Unable to switch to the text domain");
+    }
+}
